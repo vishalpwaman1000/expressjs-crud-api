647f30117dead867b0778e10f663306c19bfaed3
Vishal Waman <vishalpwaman123@gmail.com>
2025-10-30 18:17:40 +0530

    Remove unwanted file

--- src/config/db.config.js
+++
@@ -1,9 +0,0 @@
-// Deprecated: replaced by Prisma client (src/prisma/client.js)
-// Kept for compatibility in case some code still imports it; it will
-// throw if used so you can find remaining references.
-
-module.exports = new Proxy({}, {
-  get() {
-    throw new Error('db.config.js is deprecated. Use src/prisma/client.js (Prisma) instead.');
-  }
-});
